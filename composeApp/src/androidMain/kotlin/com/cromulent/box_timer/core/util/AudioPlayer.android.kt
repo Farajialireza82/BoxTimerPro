a41fff89719360928d764767b80e4e1923e24cd5
Farajialireza82 <farajialireza82@gmail.com>
2025-10-04 15:25:49 +0330

    FIX => Fixed android audio player not playing audio after closing timer screen

--- composeApp/src/androidMain/kotlin/com/cromulent/box_timer/core/util/AudioPlayer.android.kt
+++ composeApp/src/androidMain/kotlin/com/cromulent/box_timer/core/util/AudioPlayer.android.kt
@@ -1,26 +1,33 @@
 package com.cromulent.box_timer.core.util
 
 import android.content.Context
-import androidx.media3.common.MediaItem
-import androidx.media3.exoplayer.ExoPlayer
+import android.media.MediaPlayer
 import boxtimerpro.composeapp.generated.resources.Res
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
-actual class AudioPlayer(context: Context){
-    private val mediaPlayer = ExoPlayer.Builder(context).build()
-
-    init {
-        mediaPlayer.prepare()
-    }
+actual class AudioPlayer(private val context: Context) {
+    private var mediaPlayer = MediaPlayer()
 
     @OptIn(ExperimentalResourceApi::class)
     actual fun playSound(uri: String) {
-        mediaPlayer.setMediaItem(MediaItem.fromUri(Res.getUri(uri)))
-        mediaPlayer.play()
+        mediaPlayer = MediaPlayer()
+        try {
+            val assetPath = Res.getUri(uri).removePrefix("file:///android_asset/")
+            val afd = context.assets.openFd(assetPath)
+
+            mediaPlayer.apply {
+                reset()
+                setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
+                prepare()
+                start()
+            }
+            afd.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
-    actual fun release(){
+    actual fun release() {
         mediaPlayer.release()
     }
-
 }
