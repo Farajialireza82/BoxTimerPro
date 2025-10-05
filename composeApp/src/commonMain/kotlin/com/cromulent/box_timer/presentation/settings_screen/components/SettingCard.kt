eef3a9b85b121f8f3674e864da21c6943d206b50
Farajialireza82 <farajialireza82@gmail.com>
2025-10-05 17:16:55 +0330

    OPT => Improved SettingCard hover state's rounded corners

--- composeApp/src/commonMain/kotlin/com/cromulent/box_timer/presentation/settings_screen/components/SettingCard.kt
+++ composeApp/src/commonMain/kotlin/com/cromulent/box_timer/presentation/settings_screen/components/SettingCard.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -32,6 +33,7 @@ fun SettingCard(
 ) {
     Card(
         modifier = modifier
+            .clip(RoundedCornerShape(16.dp))
             .clickable(onClick != null, onClick = { onClick?.invoke() })
             .fillMaxWidth(),
         shape = RoundedCornerShape(16.dp),
