5c26d1f17f1a090de70b493b5cf6462301a1e716
Farajialireza82 <farajialireza82@gmail.com>
2025-10-13 16:25:12 +0330

    FIX => fixed timer screens flickering on start issue

--- composeApp/src/commonMain/kotlin/com/cromulent/box_timer/presentation/timer_screen/TimerViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/cromulent/box_timer/presentation/timer_screen/TimerViewModel.kt
@@ -43,6 +43,7 @@ class TimerViewModel(
                 }
             }
             appSettings = settingsRepository.getAppSettings() ?: AppSettings()
+            delay(300)
             start()
         }
     }
