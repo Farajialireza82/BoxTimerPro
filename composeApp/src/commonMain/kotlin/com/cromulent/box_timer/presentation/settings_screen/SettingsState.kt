76547b85cb3e95975497e16e46546e2b5a09c2ea
Farajialireza82 <farajialireza82@gmail.com>
2025-10-13 10:53:19 +0330

    FEAT => Added and removed some audio files

--- composeApp/src/commonMain/kotlin/com/cromulent/box_timer/presentation/settings_screen/SettingsState.kt
+++ composeApp/src/commonMain/kotlin/com/cromulent/box_timer/presentation/settings_screen/SettingsState.kt
@@ -5,16 +5,11 @@ import com.cromulent.box_timer.domain.AppSettings
 
 data class SettingsState(
     val appSettings: AppSettings = AppSettings(),
-    val audioFiles: List<AudioFile> = listOf(
-        AudioFile("Beep", "files/beep.mp3"),
-        AudioFile("Single Bell", "files/bell-single.mp3"),
-        AudioFile("Bell Three Times", "files/bell-three-times.mp3"),
-        AudioFile("Mute", null),
-    ),
     val startRoundAudioFiles: List<AudioFile> = listOf(
         AudioFile("Bell Single", "files/bell-single.mp3"),
         AudioFile("Bell Three Times", "files/bell-three-times.mp3"),
         AudioFile("Whistle", "files/whistle.mp3"),
+        AudioFile("🗣️FIGHT Deep Voice", "files/fight-deep-voice.mp3"),
         AudioFile("Air Horn Long", "files/air-horn-long.mp3"),
         AudioFile("Air Horn Short", "files/air-horn-short.mp3"),
         AudioFile("Buzzer Long", "files/buzzer-long.mp3"),
@@ -38,7 +33,6 @@ data class SettingsState(
     val countDownAudioFiles: List<AudioFile> = listOf(
         AudioFile("Beep", "files/beep.mp3"),
         AudioFile("Barcode", "files/Barcode-Scanner.mp3"),
-        AudioFile("Car Horn", "files/Car-Horn.mp3"),
         AudioFile("Clock Beep", "files/Clock-Beep.mp3"),
         AudioFile("Electric Beep", "files/Electric-Beep.mp3"),
         AudioFile("Drop", "files/Drop.mp3"),
