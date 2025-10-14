dfefd010b578093b25a5312cbb6563c5a3dcbb11
Farajialireza82 <farajialireza82@gmail.com>
2025-10-14 18:56:20 +0330

    Version Number Upgrade

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -81,7 +81,7 @@ android {
         applicationId = "com.cromulent.box_timer"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
+        versionCode = 2
         versionName = "${project.findProperty("PROJECT_VERSION")}"
     }
     packaging {
