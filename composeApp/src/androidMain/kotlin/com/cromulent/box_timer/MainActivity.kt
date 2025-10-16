99cae3125c0d9397bb29dde539b3ecb4a3595c8b
Farajialireza82 <farajialireza82@gmail.com>
2025-10-16 10:42:19 +0330

    FIX => Fixed status bar color being wrong when device is in light mode

--- composeApp/src/androidMain/kotlin/com/cromulent/box_timer/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/cromulent/box_timer/MainActivity.kt
@@ -5,9 +5,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.cromulent.box_timer.core.app.App
 import com.cromulent.box_timer.BuildKonfig.YANDEX_METRICA_API_KEY
+import com.cromulent.box_timer.presentation.theme.BoxTimerProTheme
 import io.appmetrica.analytics.AppMetrica
 import io.appmetrica.analytics.AppMetricaConfig
 import org.koin.core.context.loadKoinModules
@@ -31,7 +47,18 @@ class MainActivity : ComponentActivity() {
         })
 
         setContent {
-            App()
+            BoxTimerProTheme {
+                val view = LocalView.current
+                val colorScheme = MaterialTheme.colorScheme
+                
+                SideEffect {
+                    val window = (view.context as Activity).window
+                    window.statusBarColor = colorScheme.background.toArgb()
+                    WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+                }
+                
+                App()
+            }
         }
         Thread {
             Thread.sleep(500)
